eb1096c6321fad857a9d0b79ee8e68d240dc5608
JAldo1980 <jamesalderman1980@gmail.com>
2024-02-13 10:55:20 +0000

    updated state using the spread operator

--- state-practice/src/App.jsx
+++ state-practice/src/App.jsx
@@ -2,12 +2,28 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [state, setState] = useState(0);
+
+  function handlePlus() {
+    setState((prevCount) => prevCount + 1);
+  }
+
+  function handleMinus() {
+    setState((prevCount) => prevCount - 1);
+  }
 
   return (
-    <>
-      <h1>TEST</h1>
-    </>
+    <div className="counter">
+      <button className="counter--minus" onClick={handleMinus}>
+        –
+      </button>
+      <div className="counter--count">
+        <h1>{state}</h1>
+      </div>
+      <button className="counter--plus" onClick={handlePlus}>
+        +
+      </button>
+    </div>
   );
 }
 
